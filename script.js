006ff69bcddef5eabf363b5b6f845ef8722fcecd
Asgharkhattak05 <arhumkhattak05@gmail.com>
2025-01-09 21:50:52 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const main = document.getElementById("main");
+const cursor = document.getElementById("cursor");
+
+main.addEventListener("mousemove", function (dets) {
+  gsap.to(cursor, {
+    x: dets.x,
+    y: dets.y,
+    duration: 1,
+    ease: "back.out(1.7)",
+  });
+});

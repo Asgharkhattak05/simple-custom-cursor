f1bd72943fe82fc98856118365487ed2bcf80c49
Asgharkhattak05 <arhumkhattak05@gmail.com>
2025-01-20 20:24:29 +0500

    final

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const main = document.getElementById("main");
 const cursor = document.getElementById("cursor");
+const image = document.getElementById("image");
 
 main.addEventListener("mousemove", function (dets) {
   gsap.to(cursor, {
@@ -7,5 +8,18 @@ main.addEventListener("mousemove", function (dets) {
     y: dets.y,
     duration: 1,
     ease: "back.out(1.7)",
+  });
+});
+image.addEventListener("mouseenter", function (dets) {
+  gsap.to(cursor, {
+    scale: 2,
+    duration: 1,
+  });
+});
+
+image.addEventListener("mouseleave", function (dets) {
+  gsap.to(cursor, {
+    scale: 1,
+    duration: 1,
   });
 });
